45516d7de45586e911d05d3b1da2c46146a7c450
kam16u810 <bokseng12@gmail.com>
2018-10-29 00:53:40 +0300

    change build files

--- build-hdr_video_merging-Desktop-Debug/ui_mainwindow.h
+++ build-hdr_video_merging-Desktop-Debug/ui_mainwindow.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'mainwindow.ui'
 **
-** Created by: Qt User Interface Compiler version 5.11.1
+** Created by: Qt User Interface Compiler version 5.11.2
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
@@ -15,6 +15,8 @@
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QScrollArea>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QWidget>
 
@@ -23,30 +25,48 @@ QT_BEGIN_NAMESPACE
 class Ui_MainWindow
 {
 public:
+    QAction *recordAction;
     QWidget *centralWidget;
+    QPushButton *recordBtn;
+    QScrollArea *scrollArea;
+    QWidget *scrollAreaWidgetContents;
     QMenuBar *menuBar;
-    QMenu *menuhello_world;
+    QMenu *menuButton;
     QStatusBar *statusBar;
 
     void setupUi(QMainWindow *MainWindow)
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QStringLiteral("MainWindow"));
-        MainWindow->resize(571, 370);
+        MainWindow->resize(677, 397);
+        recordAction = new QAction(MainWindow);
+        recordAction->setObjectName(QStringLiteral("recordAction"));
         centralWidget = new QWidget(MainWindow);
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        recordBtn = new QPushButton(centralWidget);
+        recordBtn->setObjectName(QStringLiteral("recordBtn"));
+        recordBtn->setGeometry(QRect(10, 20, 80, 23));
+        scrollArea = new QScrollArea(centralWidget);
+        scrollArea->setObjectName(QStringLiteral("scrollArea"));
+        scrollArea->setGeometry(QRect(30, 50, 591, 301));
+        scrollArea->setWidgetResizable(true);
+        scrollAreaWidgetContents = new QWidget();
+        scrollAreaWidgetContents->setObjectName(QStringLiteral("scrollAreaWidgetContents"));
+        scrollAreaWidgetContents->setGeometry(QRect(0, 0, 589, 299));
+        scrollArea->setWidget(scrollAreaWidgetContents);
         MainWindow->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(MainWindow);
         menuBar->setObjectName(QStringLiteral("menuBar"));
-        menuBar->setGeometry(QRect(0, 0, 571, 20));
-        menuhello_world = new QMenu(menuBar);
-        menuhello_world->setObjectName(QStringLiteral("menuhello_world"));
+        menuBar->setGeometry(QRect(0, 0, 677, 20));
+        menuButton = new QMenu(menuBar);
+        menuButton->setObjectName(QStringLiteral("menuButton"));
         MainWindow->setMenuBar(menuBar);
         statusBar = new QStatusBar(MainWindow);
         statusBar->setObjectName(QStringLiteral("statusBar"));
         MainWindow->setStatusBar(statusBar);
 
-        menuBar->addAction(menuhello_world->menuAction());
+        menuBar->addAction(menuButton->menuAction());
+        menuButton->addAction(recordAction);
 
         retranslateUi(MainWindow);
 
@@ -56,7 +76,9 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
-        menuhello_world->setTitle(QApplication::translate("MainWindow", "hello world", nullptr));
+        recordAction->setText(QApplication::translate("MainWindow", "Record", nullptr));
+        recordBtn->setText(QApplication::translate("MainWindow", "record", nullptr));
+        menuButton->setTitle(QApplication::translate("MainWindow", "menu", nullptr));
     } // retranslateUi
 
 };
