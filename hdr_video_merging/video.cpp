2d7463577ffc5751d4b87501bdfec4d630851b98
kam16u810 <bokseng12@gmail.com>
2018-11-04 00:30:05 +0300

    add recordered video class

---
+++ hdr_video_merging/video.cpp
@@ -0,0 +1,40 @@
+#include "video.h"
+
+Video::Video(string* path)
+{
+    this->path = path;
+    String temp(path->c_str());
+    this->cap = new VideoCapture(temp);
+}
+
+Video::~Video()
+{
+    delete this->cap;
+    this->cap->release();
+}
+
+
+int Video::get_width()
+{
+    return this->width;
+}
+
+int Video::get_height()
+{
+    return this->height;
+}
+
+int Video::get_frames()
+{
+    return this->frames;
+}
+
+string Video::get_path()
+{
+    return *this->path;
+}
+
+VideoCapture* Video::get_capture()
+{
+    return this->cap;
+}
---
+++ hdr_video_merging/video.h
@@ -0,0 +1,27 @@
+#ifndef VIDEO_H
+#define VIDEO_H
+
+#include "libs.h"
+
+class Video
+{
+public:
+    explicit Video(string* path);
+    ~Video();
+
+    string get_path();
+    int get_width();
+    int get_height();
+    int get_frames();
+    VideoCapture *get_capture();
+
+private:
+    string* path;
+    VideoCapture *cap;
+    int width;
+    int height;
+    int frames;
+};
+
+
+#endif // VIDEO_H
