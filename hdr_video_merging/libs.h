4533a51e58c15dc9c48c90746f4be9875d4f99ab
kam16u810 <bokseng12@gmail.com>
2018-11-04 00:29:29 +0300

    create libs header

---
+++ hdr_video_merging/libs.h
@@ -0,0 +1,23 @@
+#ifndef LIBS_H
+#define LIBS_H
+
+#include <iostream>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/video/video.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/viz.hpp>
+//#include <opencv2/video/background_segm.hpp>
+//#include <opencv2/imgcodecs.hpp>
+//#include <opencv2/imgproc/imgproc.hpp>
+//#include <opencv2/videoio.hpp>
+#include <opencv2/video/video.hpp>
+
+#include <QString>
+#include <string>
+
+using namespace cv;
+using namespace std;
+
+#endif // LIBS_H
