7242ab26bb4d2d9ef5fa7190b19c7dfe99ea51d3
kam16u810 <bokseng12@gmail.com>
2019-03-12 02:39:54 +0300

    remove morphological operations headers

--- hdr_video_merging/hdr.h
+++ hdr_video_merging/hdr.h
@@ -23,11 +23,6 @@ protected:
 
     Mat getMotionMap(Mat &tb1, Mat &tb2);
 
-    Mat morphClosing(Mat &src);
-    Mat erosion(Mat &img, Mat &kernel);
-    Mat dilation(Mat &img, Mat &kernel);
-    bool checkPattern(Mat &img, Mat &kernel, size_t img_i, size_t img_j, int option);
-
     Mat imgToShowTransform(Mat &img);
     Mat imgToBitmapTransform(Mat &img);
 
